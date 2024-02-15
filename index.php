ab86c09487ebded5658c58006fce7683e067c954
MENGKORNG.LY <MENGKORNG.LY@PNC.LAN>
2024-02-15 15:10:37 +0700

    added something

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 require 'utils/url.php';
-if (urlIs('/admin')) {
-    require 'routers/admin.router.php';
+
+if (urlIs("/admin")) { 
+    require "routers/admin.router.php";
+} else if (urlIs('/signin') || urlIs('/signup')) {
+    require "authentication_router.php";
+}else{
+    require 'router.php';
 }
