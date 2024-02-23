b53126e868a3387e47981ce68048ce46691c098d
MENGKORNG.LY <MENGKORNG.LY@pnc.lan>
2024-02-23 23:23:25 +0700

    [feature] create a function to get data from database PD8-72

--- database/Foodpanda.sql
+++ database/Foodpanda.sql
@@ -1,128 +1,443 @@
-CREATE TABLE `Admin` (
-  `admin_id` int UNIQUE PRIMARY KEY,
-  `admin_name` varchar(20),
-  `email` varchar(100) UNIQUE,
-  `password` varchar(50),
-  `phone` varchar(20),
-  `image` varchar(255)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 23, 2024 at 05:22 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
-CREATE TABLE `Customers` (
-  `customer_id` int PRIMARY KEY AUTO_INCREMENT,
-  `first_name` varchar(50),
-  `last_name` varchar(50),
-  `email` varchar(255) UNIQUE,
-  `password` varchar(255),
-  `phone` varchar(20),
-  `registration_date` datetime
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `Restaurants` (
-  `restaurant_id` int PRIMARY KEY AUTO_INCREMENT,
-  `admin_id` int,
-  `restaurant_name` varchar(100),
-  `owner_name` varchar(100) UNIQUE,
-  `email` varchar(255) UNIQUE,
-  `password` varchar(255),
-  `location` varchar(255),
-  `rating` int(1),
-  `opening_hours` timestamp,
-  `contact_info` varchar(20),
-  `description` varchar(255),
-  `is_open` bool DEFAULT true
-);
 
-CREATE TABLE `Deliveries` (
-  `delivery_id` int PRIMARY KEY AUTO_INCREMENT,
-  `delivery_name` varchar(50),
-  `email` varchar(255) UNIQUE,
-  `password` varchar(255),
-  `phone` varchar(20),
-  `delivery_fee` int,
-  `status` bool
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `Orders` (
-  `order_id` int PRIMARY KEY AUTO_INCREMENT,
-  `customer_id` int,
-  `delivery_id` int,
-  `address` varchar(255),
-  `order_date` datetime
-);
+--
+-- Database: `foodpanda`
+--
 
-CREATE TABLE `Order_Details` (
-  `order_detail_id` int PRIMARY KEY AUTO_INCREMENT,
-  `food_id` int,
-  `order_id` int,
-  `quantity` int
-);
+-- --------------------------------------------------------
 
-CREATE TABLE `MenuItems` (
-  `item_id` int PRIMARY KEY AUTO_INCREMENT,
-  `restaurant_id` int,
-  `cuisine` varchar(100),
-  `available_status` bool DEFAULT true
-);
+--
+-- Table structure for table `admin`
+--
 
-CREATE TABLE `Foods` (
-  `food_id` int PRIMARY KEY AUTO_INCREMENT,
-  `item_id` int,
-  `food_name` varchar(50),
-  `image` varchar(255),
-  `quantity` int NOT NULL,
-  `price` int,
-  `rating` int
-);
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_name` varchar(20) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `Comments` (
-  `comment_id` int PRIMARY KEY AUTO_INCREMENT,
-  `food_id` int,
-  `customer_id` int
-);
+--
+-- Dumping data for table `admin`
+--
 
-CREATE TABLE `Favorites` (
-  `food_id` int,
-  `customer_id` int,
-  `status` bool
-);
+INSERT INTO `admin` (`admin_id`, `admin_name`, `email`, `password`, `phone`, `image`) VALUES
+(20, 'mengkorng ly', 'mengkorng.ly@student.passerellesnumeriques.org', '$2y$10$tmaOew/ueRsQRAGy5Kahl.Ab4jb2TCtdpGHo6WB6Ma..LQHKsEbIm', '+1 (373) 603-4309', 'photo_2023-11-03_22-01-30.jpg');
 
-CREATE TABLE `Payments` (
-  `payment_id` int PRIMARY KEY AUTO_INCREMENT,
-  `customer_id` int,
-  `order_detail_id` int,
-  `payment_date` datetime,
-  `status` bool
-);
+-- --------------------------------------------------------
 
-CREATE TABLE `Delivery_Notifications` (
-  `noti_id` int PRIMARY KEY AUTO_INCREMENT,
-  `order_id` int
-);
+--
+-- Table structure for table `comments`
+--
 
-ALTER TABLE `Restaurants` ADD FOREIGN KEY (`admin_id`) REFERENCES `Admin` (`admin_id`);
+CREATE TABLE `comments` (
+  `comment_id` int(11) NOT NULL,
+  `food_id` int(11) DEFAULT NULL,
+  `customer_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `Orders` ADD FOREIGN KEY (`customer_id`) REFERENCES `Customers` (`customer_id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `Orders` ADD FOREIGN KEY (`delivery_id`) REFERENCES `Deliveries` (`delivery_id`);
+--
+-- Table structure for table `customers`
+--
 
-ALTER TABLE `Order_Details` ADD FOREIGN KEY (`food_id`) REFERENCES `Foods` (`food_id`);
+CREATE TABLE `customers` (
+  `customer_id` int(11) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `registration_date` datetime DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `Order_Details` ADD FOREIGN KEY (`order_id`) REFERENCES `Orders` (`order_id`);
+--
+-- Dumping data for table `customers`
+--
 
-ALTER TABLE `MenuItems` ADD FOREIGN KEY (`restaurant_id`) REFERENCES `Restaurants` (`restaurant_id`);
+INSERT INTO `customers` (`customer_id`, `first_name`, `last_name`, `email`, `password`, `registration_date`, `image`) VALUES
+(43, 'mengkorng', 'ly', 'mengkorng.ly@student.passerellesnumeriques.org', '$2y$10$/qo5m9hQIvVuTIDyroAEaeBVdPE3PZMexOBQQ519aEAtOswfWOxnm', '2024-02-21 03:56:46', 'photo_2023-11-03_22-01-30.jpg'),
+(45, 'mengkorng', 'meng', 'lymengkorng101@gmail.com', '$2y$10$hFAwTGFvJlaNoh1eldvvGOdrLiBW2MbK4pKaYA6bi7F1Yei5XUdRa', '2024-02-22 01:31:16', 'photo_2023-12-21_12-13-56.jpg'),
+(46, 'mengkorng', 'ly', 'fowo@mailinator.com', '$2y$10$2EJEc27PfusoCffYoKsGv.bVFCa/X6R3YVyb88mokxady33WX0Xmm', '2024-02-22 14:43:49', 'coding-924920_1280.jpg');
 
-ALTER TABLE `Foods` ADD FOREIGN KEY (`item_id`) REFERENCES `MenuItems` (`item_id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `Comments` ADD FOREIGN KEY (`food_id`) REFERENCES `Foods` (`food_id`);
+--
+-- Table structure for table `deliveries`
+--
 
-ALTER TABLE `Comments` ADD FOREIGN KEY (`customer_id`) REFERENCES `Customers` (`customer_id`);
+CREATE TABLE `deliveries` (
+  `delivery_id` int(11) NOT NULL,
+  `delivery_name` varchar(50) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `delivery_fee` int(11) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `Favorites` ADD FOREIGN KEY (`food_id`) REFERENCES `Foods` (`food_id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `Favorites` ADD FOREIGN KEY (`customer_id`) REFERENCES `Customers` (`customer_id`);
+--
+-- Table structure for table `delivery_notifications`
+--
 
-ALTER TABLE `Payments` ADD FOREIGN KEY (`customer_id`) REFERENCES `Customers` (`customer_id`);
+CREATE TABLE `delivery_notifications` (
+  `noti_id` int(11) NOT NULL,
+  `order_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `Payments` ADD FOREIGN KEY (`order_detail_id`) REFERENCES `Order_Details` (`order_detail_id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `Delivery_Notifications` ADD FOREIGN KEY (`order_id`) REFERENCES `Orders` (`order_id`);
+--
+-- Table structure for table `favorites`
+--
+
+CREATE TABLE `favorites` (
+  `food_id` int(11) DEFAULT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `foods`
+--
+
+CREATE TABLE `foods` (
+  `food_id` int(11) NOT NULL,
+  `item_id` int(11) DEFAULT NULL,
+  `food_name` varchar(50) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` int(11) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menuitems`
+--
+
+CREATE TABLE `menuitems` (
+  `item_id` int(11) NOT NULL,
+  `restaurant_id` int(11) DEFAULT NULL,
+  `cuisine` varchar(100) DEFAULT NULL,
+  `available_status` tinyint(1) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `delivery_id` int(11) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `order_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_details`
+--
+
+CREATE TABLE `order_details` (
+  `order_detail_id` int(11) NOT NULL,
+  `food_id` int(11) DEFAULT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `payment_id` int(11) NOT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `order_detail_id` int(11) DEFAULT NULL,
+  `payment_date` datetime DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurants`
+--
+
+CREATE TABLE `restaurants` (
+  `restaurant_id` int(11) NOT NULL,
+  `admin_id` int(11) DEFAULT NULL,
+  `restaurant_name` varchar(100) DEFAULT NULL,
+  `owner_name` varchar(100) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `rating` int(1) DEFAULT NULL,
+  `opening_hours` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `contact_info` varchar(20) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `is_open` tinyint(1) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `food_id` (`food_id`),
+  ADD KEY `customer_id` (`customer_id`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customer_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `deliveries`
+--
+ALTER TABLE `deliveries`
+  ADD PRIMARY KEY (`delivery_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `delivery_notifications`
+--
+ALTER TABLE `delivery_notifications`
+  ADD PRIMARY KEY (`noti_id`),
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `favorites`
+--
+ALTER TABLE `favorites`
+  ADD KEY `food_id` (`food_id`),
+  ADD KEY `customer_id` (`customer_id`);
+
+--
+-- Indexes for table `foods`
+--
+ALTER TABLE `foods`
+  ADD PRIMARY KEY (`food_id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- Indexes for table `menuitems`
+--
+ALTER TABLE `menuitems`
+  ADD PRIMARY KEY (`item_id`),
+  ADD KEY `restaurant_id` (`restaurant_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `delivery_id` (`delivery_id`);
+
+--
+-- Indexes for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD PRIMARY KEY (`order_detail_id`),
+  ADD KEY `food_id` (`food_id`),
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`payment_id`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `order_detail_id` (`order_detail_id`);
+
+--
+-- Indexes for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  ADD PRIMARY KEY (`restaurant_id`),
+  ADD UNIQUE KEY `owner_name` (`owner_name`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `admin_id` (`admin_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+
+--
+-- AUTO_INCREMENT for table `deliveries`
+--
+ALTER TABLE `deliveries`
+  MODIFY `delivery_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `delivery_notifications`
+--
+ALTER TABLE `delivery_notifications`
+  MODIFY `noti_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `foods`
+--
+ALTER TABLE `foods`
+  MODIFY `food_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `menuitems`
+--
+ALTER TABLE `menuitems`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order_details`
+--
+ALTER TABLE `order_details`
+  MODIFY `order_detail_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  MODIFY `restaurant_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`food_id`) REFERENCES `foods` (`food_id`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`);
+
+--
+-- Constraints for table `delivery_notifications`
+--
+ALTER TABLE `delivery_notifications`
+  ADD CONSTRAINT `delivery_notifications_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
+
+--
+-- Constraints for table `favorites`
+--
+ALTER TABLE `favorites`
+  ADD CONSTRAINT `favorites_ibfk_1` FOREIGN KEY (`food_id`) REFERENCES `foods` (`food_id`),
+  ADD CONSTRAINT `favorites_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`);
+
+--
+-- Constraints for table `foods`
+--
+ALTER TABLE `foods`
+  ADD CONSTRAINT `foods_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `menuitems` (`item_id`);
+
+--
+-- Constraints for table `menuitems`
+--
+ALTER TABLE `menuitems`
+  ADD CONSTRAINT `menuitems_ibfk_1` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurants` (`restaurant_id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`delivery_id`) REFERENCES `deliveries` (`delivery_id`);
+
+--
+-- Constraints for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`food_id`) REFERENCES `foods` (`food_id`),
+  ADD CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
+
+--
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`),
+  ADD CONSTRAINT `payments_ibfk_2` FOREIGN KEY (`order_detail_id`) REFERENCES `order_details` (`order_detail_id`);
+
+--
+-- Constraints for table `restaurants`
+--
+ALTER TABLE `restaurants`
+  ADD CONSTRAINT `restaurants_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
