76533353a6edb61d3aaf96d8ad1940710635e647
LIEP.TOEUR <LIEP.TOEUR@pnc.lan>
2024-02-24 10:13:06 +0700

    [feature] update restaurant detail PD8-83

--- models/restaurant.model.php
+++ models/restaurant.model.php
@@ -50,3 +50,26 @@ function deletePost(int $id) : bool
     $statement->execute([':id' => $id]);
     return $statement->rowCount() > 0;
 }
+
+function updateRestaurant(string $restaurant_name, string $owner_name,string $email,string $password, string $location, int $rating, $opening_hours, string $contact_info, string $description, $is_open, int $id) : bool
+{
+    global $connection;
+    $statement = $connection->prepare("UPDATE Restaurants SET restaurant_name = :restaurant_name, owner_name = :owner_name, email = :email, password = :password, location = :location,
+    rating = :rating, opening_hours = :opening_hours, contect_ifo = :contect_info, description = :description, is_open = :is_open WHERE restaurant_id = :restaurant_id");
+    $statement->execute([
+        ':restaurant_name' => $restaurant_name,
+        ':owner_name' => $owner_name,
+        ':email' => $email,
+        ':password' => $password,
+        ':location' => $location,
+        ':rating' => $rating,
+        ':opening_hours' => $opening_hours,
+        ':contact_info' => $contact_info,
+        ':description' => $description,
+        ':is_open' => $is_open,
+        ':restaurant_id' => $id
+
+    ]);
+
+    return $statement->rowCount() > 0;
+}
