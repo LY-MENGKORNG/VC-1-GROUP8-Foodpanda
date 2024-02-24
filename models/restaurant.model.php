b96e2a90053376f01628019075df8c2c54744851
LIEP.TOEUR <LIEP.TOEUR@PNC.LAN>
2024-02-24 12:02:45 +0700

    [feature] update restaurant details PD8-83

--- models/restaurant.model.php
+++ models/restaurant.model.php
@@ -1,75 +1,20 @@
-<?php
-
-function createPost(string $title, string $description) : bool
-{
+<?php 
+function updateRestaurantDetail($restaurant_id, $restaurant_name, $owner_name, $email, $password, $location, $rating, $opening_hour, $contect_info, $description,$is_open){
     global $connection;
-    $statement = $connection->prepare("insert into posts (title, description) values (:title, :description)");
-    $statement->execute([
-        ':title' => $title,
-        ':description' => $description
-
+    $stmt = $connection->prepare("UPDATE restaurants SET restaurant_name = :restaurant_name, owner_name = :owner_name, email = :email, password = :password, location = :location, rating = :rating, 
+    opening_hour = :opening_hour, contect_info = :contect_info, description = :description, is_open = :is_open WHERE restaurant_id = :restaurant_id");
+    $stmt->execute([
+        ":restaurant_name" => $restaurant_name,
+        ":owner_name" => $owner_name,
+        ":email" => $email,
+        ":password" => $password,
+        ":location" => $location,
+        ":rating" => $rating,
+        ":opening_hour" => $opening_hour,
+        ":contect_info" => $contect_info,
+        ":description" => $description,
+        ":is_open" => $is_open,
+        ":restaurant_id" => $restaurant_id
     ]);
-
-    return $statement->rowCount() > 0;
-}
-
-function getPost(int $id) : array
-{
-    global $connection;
-    $statement = $connection->prepare("select * from posts where id = :id");
-    $statement->execute([':id' => $id]);
-    return $statement->fetch();
-}
-
-function getPosts() : array
-{
-    global $connection;
-    $statement = $connection->prepare("select * from posts");
-    $statement->execute();
-    return $statement->fetchAll();
-}
-
-function updatePost(string $title, string $description, int $id) : bool
-{
-    global $connection;
-    $statement = $connection->prepare("update posts set title = :title, description = :description where id = :id");
-    $statement->execute([
-        ':title' => $title,
-        ':description' => $description,
-        ':id' => $id
-
-    ]);
-
-    return $statement->rowCount() > 0;
-}
-
-function deletePost(int $id) : bool
-{
-    global $connection;
-    $statement = $connection->prepare("delete from posts where id = :id");
-    $statement->execute([':id' => $id]);
-    return $statement->rowCount() > 0;
-}
-
-function updateRestaurant(string $restaurant_name, string $owner_name,string $email,string $password, string $location, int $rating, $opening_hours, string $contact_info, string $description, $is_open, int $id) : bool
-{
-    global $connection;
-    $statement = $connection->prepare("UPDATE Restaurants SET restaurant_name = :restaurant_name, owner_name = :owner_name, email = :email, password = :password, location = :location,
-    rating = :rating, opening_hours = :opening_hours, contect_ifo = :contect_info, description = :description, is_open = :is_open WHERE restaurant_id = :restaurant_id");
-    $statement->execute([
-        ':restaurant_name' => $restaurant_name,
-        ':owner_name' => $owner_name,
-        ':email' => $email,
-        ':password' => $password,
-        ':location' => $location,
-        ':rating' => $rating,
-        ':opening_hours' => $opening_hours,
-        ':contact_info' => $contact_info,
-        ':description' => $description,
-        ':is_open' => $is_open,
-        ':restaurant_id' => $id
-
-    ]);
-
-    return $statement->rowCount() > 0;
+    return $stmt-> rowCount() > 0;
 }
