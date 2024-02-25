27cf7a523fac84e58755a9bb903fe0939f07010a
thary.oeun <thary.oeun@student.passerellernumeriques.org>
2024-02-25 12:13:38 +0700

    [feature] new_user_registrtion PD8-74

--- layouts/admin/navbar.php
+++ layouts/admin/navbar.php
@@ -123,7 +123,7 @@
                         </ul>
                     </li>
                     <li>
-                        <a class="show-cat-btn" href="/controllers/users/new_user_registration.users.php">
+                        <a class="show-cat-btn" href="controllers/users/new_user.registration.user.php">
                             <span class="icon user-3" aria-hidden="true"></span>Users
                             <span class="category__btn transparent-btn" title="Open list">
                                 <span class="sr-only">Open list</span>
@@ -132,7 +132,7 @@
                         </a>
                         <ul class="cat-sub-menu">
                             <li>
-                                <a href="/views/users/new_user_registration.user.php">Users-01</a> 
+                                <a href="views/users/new_user_registration.users.php">Users-01</a> 
                                 <!-- users-01.html -->
                             </li>
                             <li>
--- models/users.model.php
+++ models/users.model.php
@@ -6,3 +6,25 @@ function getUser(string $user): array
     $stmt->execute();
     return $stmt->fetchAll();
 }
+
+
+function rejectNewUserRegistration($user){
+    if (isset($_POST['reject'])) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function approveNewUserRegistration($user){
+    getAdmin();
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (isset($_POST['approve'])) {
+            $user = $_POST['user'];
+            approveNewUserRegistration($user);
+        } else if (isset($_POST['reject'])) {
+            $user = $_POST['user'];
+            rejectNewUserRegistration($user);
+        }
+    }
+}
