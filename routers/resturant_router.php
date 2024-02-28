35b637c22a350b556d9b18ef79e173bad9b36a00
DAVIT.CHOEUN <DAVIT.CHOEUN@PNC.LAN>
2024-02-28 09:58:17 +0700

    [feature] create home page PD8-110

--- routers/resturant_router.php
+++ routers/resturant_router.php
@@ -3,12 +3,14 @@ session_start();
 require "./models/restaurant.model.php";
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-if (isset($_SESSION["restaurant"])) {
-    $restaurant = $_SESSION["restaurant"];
+// if (isset($_SESSION["restaurant"])) {
+//     $restaurant = $_SESSION["restaurant"];
 
     $page = "";
     $routes = [
-        '/restaurant' => 'controllers/restaurant_owner/restaurant_owner.controller.php',
+        '/restaurant' => 'controllers/restaurant/home/home.controller.php',
+        '/restaurant/category' => 'controllers/restaurant/menu_item/menu_item.controller.php',
+        '/restaurant/delivery' => 'controllers/restaurant/delivery/delivery.controller.php',
     ];
 
     if (array_key_exists($uri, $routes)) {
@@ -20,8 +22,9 @@ if (isset($_SESSION["restaurant"])) {
 
     require "./layouts/restaurant/header.php";
     require "./layouts/restaurant/navbar.php";
+    require "./layouts/restaurant/navbar2.php";
     require $page;
     require "./layouts/restaurant/footer.php";
-}else {
-    header("Location: /restaurant/signin");
-}
+// }else {
+//     header("Location: /restaurant/signin");
+// }
