00a46491783503f78fe6fe1552cd59a71203d00e
MENGKORNG.LY <MENGKORNG.LY@pnc.lan>
2024-03-02 19:49:30 +0700

    [improve] resolve conflicts PD8-119

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ require "./models/users.model.php";
 if (urlIs("/admin") || urlIs("/admin/manage_restaurant") || 
     urlIs("/admin/add_restaurant") || urlIs("/admin/check_add_restaurant") ||
     urlIs("/admin/restaurant_owner") || urlIs("/admin/check_add_restaurant_owner") ||
-    urlIs("/admin/restaurant_detail") || urlIs("/admin/edit_restaurant")) 
+    urlIs("/admin/restaurant_detail") || urlIs("/admin/edit_restaurant") ||
+    urlIs("/admin/profile")) 
 { 
     require "routers/admin_router.php";
 }
