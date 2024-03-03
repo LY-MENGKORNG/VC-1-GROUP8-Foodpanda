4fbffce45077724718f60791c46fc274dacb6cb7
LIEP.TOEUR <LIEP.TOEUR@PNC.LAN>
2024-03-03 16:38:14 +0700

    [feature] add category by owner PD8-120

--- controllers/restaurant/menu_item/menu_item.controller.php
+++ controllers/restaurant/menu_item/menu_item.controller.php
@@ -1,3 +1,4 @@
 <?php
 $category = getAllUsers(3);
+var_dump($restaurant_owner);
 require "./views/restaurant/menu_item/menu_item.view.php";
--- views/restaurant/menu_item/menu_item.view.php
+++ views/restaurant/menu_item/menu_item.view.php
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-<div class="container p-4">
-    <div class="category mb-5">
-        <h2>Category</h2>
-    </div>
-    
-    <?php foreach ($category as $item) { ?>
-        <div class="card" style="width: 18rem;">
-          <img src="..." class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-item-id"><?= $item['item_id'] ?></h5>
-            <h5 class="card-restaurant-id"><?= $item['restaurant_id'] ?></h5>
-            <h5 class="card-cuisine"><?= $item['cuisine'] ?></h5>
-            <h5 class="card-available-status"><?= $item['available_status'] ?></h5>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-    <?php } ?>
-
-</div>
-
-</div> 
-</div>
-=======
 <main class="main users chart-page" id="skip-target">
     <div class="container-fluid">
         <div class="d-flex flex-row justify-content-sm-between px-2">
@@ -110,4 +86,3 @@
     </div>
 
 </main>
->>>>>>> origin/owner_add_category
