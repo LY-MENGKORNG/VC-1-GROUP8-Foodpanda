687e9031bb3226853a231aa4ba7cda345ca54141
CEO of Bathroom <141013479+chhuneii2004@users.noreply.github.com>
2024-03-06 10:13:13 +0700

    merge code from create_list_delivery

--- controllers/restaurant/menu_item/add_menu_item.controller.php
+++ controllers/restaurant/menu_item/add_menu_item.controller.php
@@ -1,6 +1,7 @@
 <?php
 $owner_id = $restaurant_owner["user_id"];
 $restaurant_id = getRestaurantByOwner($owner_id)["restaurant_id"];
+echo "hi";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!empty(($_POST["cate_name"])) && !empty($_POST["description"])) {
