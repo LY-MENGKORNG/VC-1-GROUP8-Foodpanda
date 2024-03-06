f8259b237d8432877f10f92a3949c47c5aef31e0
DAVIT.CHOEUN <DAVIT.CHOEUN@PNC.LAN>
2024-03-06 11:16:59 +0700

    [feature] delivery athentication PD8-20

--- controllers/authentication/delivery/signout/signout.controller.php
+++ controllers/authentication/delivery/signout/signout.controller.php
@@ -1,2 +1,5 @@
 <?php 
-require "./views/signout/delivery_signout.view.php";
+session_start();
+$is_restaurant_owner_email = isset($_SESSION['is_delivery_owner_email'])?$_SESSION['$is_delivery_owner_email']: "";
+$is_restaurant_owner_password = isset($_SESSION["is_delivery_owner_password"]) ? $_SESSION['is_delivery_owner_password'] : "";
+require "./views/signout/delivery_signout.php";
