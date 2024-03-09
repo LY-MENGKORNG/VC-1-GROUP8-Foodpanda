e33f80b4bf215e872ad81b9a36c517817d817793
MENGKORNG-LY <mengkorng.ly@student.passerellesnumeriques.org>
2024-03-10 02:13:58 +0700

    [feature] create delivery page PD8-133

--- views/restaurant/category/category.view.php
+++ views/restaurant/category/category.view.php
@@ -87,14 +87,18 @@
                                     <i data-feather="more-horizontal" aria-hidden="true"></i>
                                 </button>
                                 <ul class="users-cate$category-dropdown dropdown">
-                                    <li><a class="btn btn-tranparent main-title" data-bs-toggle="modal"
-                                            data-bs-target="#edit<?= $category["cate_id"] ?>">Edit</a></li>
+                                    <li>
+                                        <a class="btn btn-tranparent" data-bs-toggle="modal"
+                                            data-bs-target="#edit<?= $category["cate_id"] ?>">Edit
+                                        </a>
+                                    </li>
                                 </ul>
                             </span>
                         </td>
                     </tr>
                     <!-- edit category -->
-                    <div class="modal fade" id="edit<?= $category["cate_id"] ?>" tabindex="-1" aria-labelledby="modal-title" aria-hidden="true">
+                    <div class="modal fade" id="edit<?= $category["cate_id"] ?>" tabindex="-1" aria-labelledby="modal-title"
+                        aria-hidden="true">
                         <div class="modal-dialog">
                             <div class="modal-content p-4">
                                 <div class="modal-header">
@@ -115,14 +119,16 @@
                                             <div class="form-group col-6">
                                                 <label for="cate_name">Category Name:</label>
                                                 <input type="text" name="cate_name" id="cate_name"
-                                                    class="form-control border border-info" value="<?= $category["cate_name"] ?>" required>
+                                                    class="form-control border border-info"
+                                                    value="<?= $category["cate_name"] ?>" required>
                                             </div>
                                         </div>
                                         <div class="form-group">
                                             <label for="description">Description:</label>
                                             <textarea name="description" id="description"
                                                 class="form-control border border-info"
-                                                style="min-height: 50px; max-height:200px;" name="<?= $category["description"] ?>" required></textarea>
+                                                style="min-height: 50px; max-height:200px;"
+                                                name="<?= $category["description"] ?>" required></textarea>
                                         </div>
                                         <button type="submit" class="btn btn-primary">Submit</button>
                                     </form>
