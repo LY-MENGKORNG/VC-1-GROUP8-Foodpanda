617d757088f832d2715528e8faa0e1b6ef5c57d1
MENGKORNG-LY <mengkorng.ly@student.passerellesnumeriques.org>
2024-03-10 03:28:40 +0700

    [feature] create delivery page PD8-133

--- views/restaurant/category/category.view.php
+++ views/restaurant/category/category.view.php
@@ -137,47 +137,23 @@
                         </div>
                     </div>
                     <!-- delete category -->
-                    <div class="modal fade" id="deleteCate<?= $category["cate_id"] ?>" tabindex="-1" aria-labelledby="modal-title"
-                        aria-hidden="true">
+                    <div class="modal fade" id="deleteCate<?= $category["cate_id"] ?>" tabindex="-1" aria-labelledby="modal-title" aria-hidden="true">
                         <div class="modal-dialog">
-                            <div class="modal-content p-4">
-                                <div class="modal-header">
-                                    <h5 class="modal-title">
-                                        <h5 class="modal-title text-dark" id="modal-title">Edit Category</h5>
-                                        <button type="button" class="btn btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"><i class="feather-x" style="font-size: 20px;"></i></button>
-                                    </h5>
+                            <div class="modal-content">
+                                <div class="modal-header row p-2 m-0">
+                                    <div class="alert alert-danger col-12 rounded-0" role="alert">
+                                        Are you sure you want to delete this category?
+                                    </div>
                                 </div>
-                                <div class="modal-body text-dark">
-                                    <form action="/restaurant/edit_category" method="post" enctype="multipart/form-data">
-                                        <div class="row">
-                                            <div class="form-group col-6">
-                                                <label for="image">Image</label>
-                                                <input type="file" name="image" id="image"
-                                                    class="form-control border border-info">
-                                            </div>
-                                            <div class="form-group col-6">
-                                                <label for="cate_name">Category Name:</label>
-                                                <input type="text" name="cate_name" id="cate_name"
-                                                    class="form-control border border-info"
-                                                    value="<?= $category["cate_name"] ?>" required>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="description">Description:</label>
-                                            <textarea name="description" id="description"
-                                                class="form-control border border-info"
-                                                style="min-height: 50px; max-height:200px;" required><?= $category["description"] ?></textarea>
-                                        </div>
-                                        <input type="hidden" name="cate_id" value="<?= $category["cate_id"] ?>">
-                                        <input type="hidden" name="current_img" value="<?= $category["cate_img"] ?>">
-                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                <div class="modal-body p-2 m-2 d-flex justify-content-end">
+                                    <button type="button" class="btn btn-secondary mx-2" data-bs-dismiss="modal">Close</button>
+                                    <form action="/restaurant/delete_cate" method="post">
+                                        <button type="submit" name="cate_id" value="<?= $category["cate_id"] ?>" class="btn btn-primary">Save changes</button>
                                     </form>
                                 </div>
                             </div>
                         </div>
                     </div>
-
                 <?php } ?>
             </tbody>
         </table>
