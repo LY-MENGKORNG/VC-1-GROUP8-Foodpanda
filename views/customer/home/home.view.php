5c988a3a78904dd05df5c8f99c3d13899ef08d37
MENGKORNG-LY <mengkorng.ly@student.passerellesnumeriques.org>
2024-03-11 18:50:42 +0700

    [feature] completed feature search product in customer page PD8-12

--- views/customer/home/home.view.php
+++ views/customer/home/home.view.php
@@ -68,56 +68,6 @@
         </div>
     </div>
     <div class="container">
-        <div class="pt-4 pb-2 title d-flex align-items-center">
-            <h5 class="m-0">Trending this week</h5>
-            <a class="font-weight-bold ml-auto" href="trending.html">View all <i class="feather-chevrons-right"></i></a>
-        </div>
-
-        <div class="trending-slider">
-            <?php foreach ($foods as $food) { ?>
-                <form action="/customer/restaurant" method="post">
-                    <div class="osahan-slider-item">
-                        <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
-                            <div class="list-card-image">
-                                <div class="star position-absolute">
-                                    <span class="badge badge-success"><i class="feather-star"></i> 3.1 (300+)</span>
-                                </div>
-                                <div class="favourite-heart text-danger position-absolute">
-                                    <a href="#"><i class="feather-heart"></i></a>
-                                </div>
-                                <div class="member-plan position-absolute">
-                                    <span class="badge badge-dark">Promoted</span>
-                                </div>
-                                <label for="toRestaurant" id="food">
-                                    <img alt="#" src="../../../assets/images/uploads/restaurants/foods/<?= $food["image"] ?>" class="img-fluid item-img w-100" />
-                                </label>
-                            </div>
-                            <div class="p-3 position-relative">
-                                <div class="list-card-body">
-                                    <h6 class="mb-1">
-                                        <label for="toRestaurant" class="text-black"><?= $food["restaurant_name"] ?>
-                                        </label>
-                                    </h6>
-                                    <p class="text-gray mb-3"><?= $food["cate_name"] ?></p>
-                                    <p class="text-gray mb-3 time">
-                                        <span class="bg-light text-dark rounded-sm pl-2 pb-1 pt-1 pr-2"><i class="feather-clock"></i> 15–30
-                                            min</span>
-                                        <span class="float-right text-black-50"> $<?= intval($food["price"]) * 2 ?> FOR TWO</span>
-                                    </p>
-                                </div>
-                                <div class="list-card-badge">
-                                    <span class="badge badge-danger">OFFER</span>
-                                    <small> Use Coupon OSAHAN50</small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <input id="food_id" type="hidden" name="" value="">
-                    <button id="toRestaurant" value="<?= $food["food_id"] ?>" type="submit" style="display: none;"></button>
-                </form>
-            <?php } ?>
-
-        </div>
 
         <div class="py-3 title d-flex align-items-center">
             <h5 class="m-0">Most popular</h5>
