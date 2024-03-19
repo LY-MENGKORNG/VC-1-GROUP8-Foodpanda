12a64dc083d060f2664214b2d044ee0859c6ab27
MENGKORNG-LY <mengkorng.ly@student.passerellesnumeriques.org>
2024-03-19 18:00:22 +0700

    [feature] customer order product PD8-15

--- views/restaurant/food/food.view.php
+++ views/restaurant/food/food.view.php
@@ -2,7 +2,8 @@
     <div class="container-fluid">
         <div class="d-flex flex-row justify-content-sm-between px-2">
             <h1 class="main-title col-9">List Foods</h1>
-            <button class="btn btn-outline-primary col-2 font-weight-bold text-gray rounded" data-bs-toggle="modal" data-bs-target="#createFood">
+            <button class="btn btn-outline-primary col-2 font-weight-bold text-gray rounded" data-bs-toggle="modal"
+                data-bs-target="#createFood">
                 Add Food
             </button>
         </div>
@@ -19,7 +20,8 @@
                 </div>
                 <div class="modal-body">
                     <form action="/restaurant/add_food" method="post" enctype="multipart/form-data">
-                        <div class="form-group m-auto border border-info rounded-circle d-flex justify-content-center align-items-center" style="width: 100px; height: 100px;">
+                        <div class="form-group m-auto border border-info rounded-circle d-flex justify-content-center align-items-center"
+                            style="width: 100px; height: 100px;">
                             <input type="file" name="image" id="image" style="display: none;" required>
                             <label for="image">
                                 <i class="feather-image text-primary" style="font-size: 50px;"></i>
@@ -28,31 +30,37 @@
                         <div class="row d-flex gap-3 mt-4">
                             <div class="form-group col-6">
                                 <label for="food_name">Food Name:</label>
-                                <input type="text" name="food_name" id="food_name" class="form-control border border-info" required>
+                                <input type="text" name="food_name" id="food_name"
+                                    class="form-control border border-info" required>
                             </div>
                             <div class="row d-flex gap-3 mt-4">
                                 <div class="form-group col-6">
                                     <label for="food_name">Food Name:</label>
-                                    <input type="text" name="food_name" id="food_name" class="form-control border border-info" required>
+                                    <input type="text" name="food_name" id="food_name"
+                                        class="form-control border border-info" required>
                                 </div>
                                 <div class="form-group col-6">
                                     <label for="price">Price:</label>
-                                    <input type="number" name="price" id="price" min="0" class="form-control border border-info" required>
+                                    <input type="number" name="price" id="price" min="0"
+                                        class="form-control border border-info" required>
                                 </div>
                             </div>
                             <div class="row d-flex gap-3">
                                 <div class="form-group col-6">
                                     <label for="quantity">Quantity:</label>
-                                    <input type="number" name="quantity" id="quantity" min="0" class="form-control border border-info" required>
+                                    <input type="number" name="quantity" id="quantity" min="0"
+                                        class="form-control border border-info" required>
                                 </div>
                                 <div class="form-group col-6">
                                     <label for="rating">Rating:</label>
-                                    <input type="number" name="rating" id="rating" min="0" max="5" class="form-control border border-info" required>
+                                    <input type="number" name="rating" id="rating" min="0" max="5"
+                                        class="form-control border border-info" required>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label for="cate">Category:</label>
-                                <select name="cate_id" id="cate" class="form-control form-select-md border border-info" required>
+                                <select name="cate_id" id="cate" class="form-control form-select-md border border-info"
+                                    required>
                                     <?php foreach ($categories as $category) { ?>
                                         <option value="<?= $category["cate_id"] ?>">
                                             <?= $category["cate_name"] ?>
@@ -72,87 +80,119 @@
     <div class="users-table table-wrapper mt-4 p-4">
         <table class="posts-table rounded-3">
             <thead>
-                <tr class="users-table-info  bg-transparent">
-                    <th>
-                        <label class="users-table__checkbox ms-20">
-                            <input type="checkbox" class="check-all">
-                            Image
-                        </label>
-                        </td>
-                    <td><?= $item["food_name"] ?></td>
-                    <td><?= $item["price"] ?>$</td>
-                    <td><?= $item["quantity"] ?></td>
-                    <td><?= $item["rating"] ?></td>
-                    <td>
-                        <span class="p-relative">
-                            <button class="dropdown-btn transparent-btn" type="button" title="More info">
-                                <div class="sr-only">More info</div>
-                                <i data-feather="more-horizontal" aria-hidden="true"></i>
-                            </button>
-                            <ul class="users-item-dropdown dropdown">
-                                <li><a class="btn btn-tranparent" data-bs-toggle="modal" data-bs-target="#editfood<?= $item['food_id'] ?>">Edit</a></li>
-                                <li><a class="btn btn-tranparent" data-bs-toggle="modal" data-bs-target="#deletefood<?= $item['food_id'] ?>">Trash</a></li>
-                            </ul>
-                        </span>
-                    </td>
+                <tr>
+                    <th>Image</th>
                 </tr>
-                <!-- edit food -->
-                <div class="modal fade text-dark" id="editfood<?= $item["food_id"] ?>" tabindex="-1" aria-labelledby="modal-title" aria-hidden="true">
-                    <div class="modal-dialog modal-lg">
-                        <div class="modal-content p-3">
-                            <div class="modal-header">
-                                <h5 class="modal-title">
-                                    <h5 class="modal-title" id="modal-title">Edit Food</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </h5>
-                            </div>
-                            <div class="modal-body">
-                                <form action="/restaurant/edit_food" method="post" enctype="multipart/form-data">
-                                    <div class="form-group m-auto border border-info rounded-circle d-flex justify-content-center align-items-center" style="width: 100px; height: 100px;">
-                                        <input type="hidden" name="current_image" value="<?= $item["image"] ?>">
-                                        <input type="hidden" name="food_id" value="<?= $item["food_id"] ?>">
-                                        <input type="file" name="food_img" id="food_img" style="display: none;">
-                                        <label for="food_img">
-                                            <i class="feather-image text-primary" style="font-size: 50px;"></i>
-                                        </label>
-                                    </div>
-                                    <div class="row d-flex gap-3 mt-4">
-                                        <div class="form-group col-6">
-                                            <label for="food_name">Food Name:</label>
-                                            <input type="text" name="food_name" id="food_name" class="form-control border border-info" value="<?= $item["food_name"] ?>" required>
+            </thead>
+            <tbody>
+                <?php foreach ($foods as $item) { ?>
+                    <tr class="users-table-info ">
+                        <td>
+                            <label class="users-table__checkbox ms-20">
+                                <input type="checkbox" class="check-all">
+                                Image
+                            </label>
+                        </td>
+                        <td>
+                            <?= $item["food_name"] ?>
+                        </td>
+                        <td>
+                            <?= $item["price"] ?>$
+                        </td>
+                        <td>
+                            <?= $item["quantity"] ?>
+                        </td>
+                        <td>
+                            <?= $item["food_rate"] ?>
+                        </td>
+                        <td>
+                            <span class="p-relative">
+                                <button class="dropdown-btn transparent-btn" type="button" title="More info">
+                                    <div class="sr-only">More info</div>
+                                    <i data-feather="more-horizontal" aria-hidden="true"></i>
+                                </button>
+                                <ul class="users-item-dropdown dropdown">
+                                    <li><a class="btn btn-tranparent" data-bs-toggle="modal"
+                                            data-bs-target="#editfood<?= $item['food_id'] ?>">Edit</a></li>
+                                    <li><a class="btn btn-tranparent" data-bs-toggle="modal"
+                                            data-bs-target="#deletefood<?= $item['food_id'] ?>">Trash</a></li>
+                                </ul>
+                            </span>
+                        </td>
+                    </tr>
+                    <!-- edit food -->
+                    <div class="modal fade text-dark" id="editfood<?= $item["food_id"] ?>" tabindex="-1"
+                        aria-labelledby="modal-title" aria-hidden="true">
+                        <div class="modal-dialog modal-lg">
+                            <div class="modal-content p-3">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">
+                                        <h5 class="modal-title" id="modal-title">Edit Food</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </h5>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="/restaurant/edit_food" method="post" enctype="multipart/form-data">
+                                        <div class="form-group m-auto border border-info rounded-circle d-flex justify-content-center align-items-center"
+                                            style="width: 100px; height: 100px;">
+                                            <input type="hidden" name="current_image" value="<?= $item["image"] ?>">
+                                            <input type="hidden" name="food_id" value="<?= $item["food_id"] ?>">
+                                            <input type="file" name="food_img" id="food_img" style="display: none;">
+                                            <label for="food_img">
+                                                <i class="feather-image text-primary" style="font-size: 50px;"></i>
+                                            </label>
                                         </div>
-                                        <div class="form-group col-6">
-                                            <label for="price">Price:</label>
-                                            <input type="number" name="price" id="price" min="0" class="form-control border border-info" value="<?= $item["price"] ?>" required>
+                                        <div class="row d-flex gap-3 mt-4">
+                                            <div class="form-group col-6">
+                                                <label for="food_name">Food Name:</label>
+                                                <input type="text" name="food_name" id="food_name"
+                                                    class="form-control border border-info"
+                                                    value="<?= $item["food_name"] ?>" required>
+                                            </div>
+                                            <div class="form-group col-6">
+                                                <label for="price">Price:</label>
+                                                <input type="number" name="price" id="price" min="0"
+                                                    class="form-control border border-info" value="<?= $item["price"] ?>"
+                                                    required>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="row d-flex gap-3">
-                                        <div class="form-group col-6">
-                                            <label for="quantity">Quantity:</label>
-                                            <input type="number" name="quantity" id="quantity" min="0" class="form-control border border-info" value="<?= $item["quantity"] ?>" required>
+                                        <div class="row d-flex gap-3">
+                                            <div class="form-group col-6">
+                                                <label for="quantity">Quantity:</label>
+                                                <input type="number" name="quantity" id="quantity" min="0"
+                                                    class="form-control border border-info" value="<?= $item["quantity"] ?>"
+                                                    required>
+                                            </div>
+                                            <div class="form-group col-6">
+                                                <label for="rating">Rating:</label>
+                                                <input type="number" name="rating" id="food_rate" min="0" max="5"
+                                                    class="form-control border border-info" value="<?= $item["food_rate"] ?>"
+                                                    required>
+                                            </div>
                                         </div>
-                                        <div class="form-group col-6">
-                                            <label for="rating">Rating:</label>
-                                            <input type="number" name="rating" id="rating" min="0" max="5" class="form-control border border-info" value="<?= $item["rating"] ?>" required>
+                                        <div class="form-group">
+                                            <label for="cate">Category:</label>
+                                            <select name="cate_id" id="cate"
+                                                class="form-control form-select-md border border-info" required>
+                                                <?php foreach ($categories as $category) { ?>
+                                                    <option value="<?= $category["cate_id"] ?>">
+                                                        <?= $category["cate_name"] ?>
+                                                    </option>
+                                                <?php } ?>
+                                            </select>
                                         </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="cate">Category:</label>
-                                        <select name="cate_id" id="cate" class="form-control form-select-md border border-info" required>
-                                            <?php foreach ($categories as $category) { ?>
-                                                <option value="<?= $category["cate_id"] ?>"><?= $category["cate_name"] ?></option>
-                                            <?php } ?>
-                                        </select>
-                                    </div>
-                                    <div class="d-flex justify-content-end ">
-                                        <button type="submit" class="btn btn-primary col-2 mt-3">Submit</button>
-                                    </div>
-                                </form>
+                                        <div class="d-flex justify-content-end ">
+                                            <button type="submit" class="btn btn-primary col-2 mt-3">Submit</button>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <!-- delete category -->
-                    <div class="modal fade" id="deletefood<?= $item["food_id"] ?>" tabindex="-1" aria-labelledby="modal-title" aria-hidden="true">
+                    <div class="modal fade" id="deletefood<?= $item["food_id"] ?>" tabindex="-1"
+                        aria-labelledby="modal-title" aria-hidden="true">
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header row p-2 m-0">
@@ -161,17 +201,18 @@
                                     </div>
                                 </div>
                                 <div class="modal-body p-2 m-2 d-flex justify-content-end">
-                                    <button type="button" class="btn btn-secondary mx-2" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-secondary mx-2"
+                                        data-bs-dismiss="modal">Close</button>
                                     <form action="/restaurant/delete_food" method="post">
-                                        <button type="submit" name="food_id" value="<?= $item["food_id"] ?>" class="btn btn-primary">Save changes</button>
+                                        <button type="submit" name="food_id" value="<?= $item["food_id"] ?>"
+                                            class="btn btn-primary">Save changes</button>
                                     </form>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <?php  ?>
-                </tbody>
+                <?php } ?>
+            </tbody>
         </table>
     </div>
 
