28222cb766af40172b243166a221f02d414974ba
MENGKORNG-LY <mengkorng.ly@student.passerellesnumeriques.org>
2024-03-19 15:04:10 +0700

    [improve] change something on list food

--- views/restaurant/food/food.view.php
+++ views/restaurant/food/food.view.php
@@ -9,7 +9,7 @@
                 </button>
             </div>
         </div>
-        <!-- Modal -->
+        <!-- Modal --> 
         <div class="modal fade" id="categoryModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden="true">
             <div class="modal-dialog modal-lg">
                 <div class="modal-content p-3">
