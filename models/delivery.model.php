bf2ea161072b13aa40413de7074defec5d469c48
MENGKORNG-LY <mengkorng.ly@student.passerellesnumeriques.org>
2024-03-19 20:09:30 +0700

    [feature] customer order product PD8-15

---
+++ models/delivery.model.php
@@ -0,0 +1,28 @@
+<?php 
+
+function getAllOrders($delivery_id) : array {
+    global $connection;
+    $stmt = $connection->prepare(
+        "SELECT orders.order_id, orders.delivery_id, orders.order_status, orders.order_date, orders.restaurant_name, orders.deliver_date, 
+        orders.restaurant_img, orders.address_id, users.first_name, users.last_name, users.email, users.profile
+        FROM users INNER JOIN orders ON orders.customer_id = users.user_id
+        WHERE orders.delivery_id = :delivery_id"
+    );
+    $stmt->execute([
+        ":delivery_id" => $delivery_id
+    ]);
+    return $stmt->fetchAll();
+}
+function getAllOrder($status, $delivery_id) : array {
+    global $connection;
+    $stmt = $connection->prepare(
+        "SELECT orders.order_id, orders.delivery_id, orders.order_status, orders.order_date, orders.restaurant_name, orders.deliver_date, 
+        orders.restaurant_img, orders.address_id, users.first_name, users.last_name, users.email, users.profile
+        FROM users INNER JOIN orders ON orders.customer_id = users.user_id WHERE order_status = :status AND delivery_id = :delivery_id"
+    );
+    $stmt->execute([
+        ":status" => $status,
+        ":delivery_id" => $delivery_id
+    ]);
+    return $stmt->fetchAll();
+}
