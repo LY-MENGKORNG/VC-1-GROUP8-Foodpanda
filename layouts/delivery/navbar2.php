45642af00b894950b5eecac4a25e24b4b9fa7f3d
MENGKORNG-LY <mengkorng.ly@student.passerellesnumeriques.org>
2024-03-19 18:15:46 +0700

    [feature] customer order product PD8-15

--- layouts/delivery/navbar2.php
+++ layouts/delivery/navbar2.php
@@ -1,18 +1,9 @@
 <div class="main-wrapper">
     <!-- ! Main nav -->
     <nav class="main-nav--bg bg-light row px-5" style="border-bottom: 1px solid lightgray;">
-        <nav class="navbar navbar-light bg-light col-6 pl-5">
-            <form class="d-flex">
-                <input class="form-control me-2 border border-gray" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+        <nav class="navbar navbar-light bg-light col-6 pl-5" style="height: 45px;">
+            
         </nav>
-        <div class="d-flex flex-row justify-content-sm-between pr-5 col-6">
-            <button class="btn btn-primary font-weight-bold text-gray rounded  ml-auto my-auto" data-bs-toggle="modal" data-bs-target="#deliveryModal" style="height: 45px;">
-                <i></i>
-                + Add Report
-            </button>
-        </div>
         <div class="modal fade" id="deliveryModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden="true">
             <div class="modal-dialog modal-lg">
                 <div class="modal-content p-4">
--- views/restaurant/food/food.view.php
+++ views/restaurant/food/food.view.php
@@ -1,37 +1,30 @@
 <main class="main users chart-page" id="skip-target">
     <div class="container-fluid">
-        <div class="d-flex flex-row justify-content-sm-between px-2">
-            <h1 class="main-title col-9">List Foods</h1>
-            <button class="btn btn-outline-primary col-2 font-weight-bold text-gray rounded" data-bs-toggle="modal"
-                data-bs-target="#createFood">
+        <div class="d-flex justify-content-between px-2">
+            <h1 class="main-title">List Foods</h1>
+            <button class="btn btn-primary btn-sm font-weight-bold text-gray rounded" data-bs-toggle="modal"
+                data-bs-target="#createFood" style="width: 130px; height: 40px;">
                 Add Food
             </button>
         </div>
-    </div>
-    <!-- Modal create -->
-    <div class="modal fade" id="createFood" tabindex="-1" aria-labelledby="modal-title" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content p-3">
-                <div class="modal-header">
-                    <h5 class="modal-title">
-                        <h5 class="modal-title" id="modal-title">Create new Food</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </h5>
-                </div>
-                <div class="modal-body">
-                    <form action="/restaurant/add_food" method="post" enctype="multipart/form-data">
-                        <div class="form-group m-auto border border-info rounded-circle d-flex justify-content-center align-items-center"
-                            style="width: 100px; height: 100px;">
-                            <input type="file" name="image" id="image" style="display: none;" required>
-                            <label for="image">
-                                <i class="feather-image text-primary" style="font-size: 50px;"></i>
-                            </label>
-                        </div>
-                        <div class="row d-flex gap-3 mt-4">
-                            <div class="form-group col-6">
-                                <label for="food_name">Food Name:</label>
-                                <input type="text" name="food_name" id="food_name"
-                                    class="form-control border border-info" required>
+        <!-- Modal --> 
+        <div class="modal fade" id="createFood" tabindex="-1" aria-labelledby="modal-title" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content p-3">
+                    <div class="modal-header"> 
+                        <h5 class="modal-title">
+                            <h5 class="modal-title" id="modal-title">Create new Food</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </h5>
+                    </div>
+                    <div class="modal-body">
+                        <form action="/restaurant/add_food" method="post" enctype="multipart/form-data">
+                            <div class="form-group m-auto border border-info rounded-circle d-flex justify-content-center align-items-center"
+                                style="width: 100px; height: 100px;">
+                                <input type="file" name="image" id="image" style="display: none;" required>
+                                <label for="image">
+                                    <i class="feather-image text-primary" style="font-size: 50px;"></i>
+                                </label>
                             </div>
                             <div class="row d-flex gap-3 mt-4">
                                 <div class="form-group col-6">
@@ -77,28 +70,37 @@
         </div>
     </div>
 
-    <div class="users-table table-wrapper mt-4 p-4">
+    <div class="users-table table-wrapper mt-4 border border-gray shadow-sm">
         <table class="posts-table rounded-3">
             <thead>
                 <tr>
-                    <th>Image</th>
+                    <th class="py-3">
+                        <label class="users-table__checkbox ms-20">
+                            <input type="checkbox" class="check-all">Image
+                        </label>
+                    </th>
+                    <th>Food Name</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Rating</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($foods as $item) { ?>
-                    <tr class="users-table-info ">
+                    <tr class="users-table-info  bg-transparent">
                         <td>
-                            <label class="users-table__checkbox ms-20">
-                                <input type="checkbox" class="check-all">
-                                Image
-                            </label>
+                        <label class="users-table__checkbox">
+                        <input type="checkbox" class="check">
+                      <div class="categories-table-img">
+                        <picture>
+                          <source srcset="../../assets/images/uploads/restaurants/foods/<?= $item["image"] ?>" type="image/webp"><img src="../../assets/images/uploads/restaurants/foods/<?= $item["image"] ?>" alt="<?= $item["food_name"] ?>">
+                        </picture>
+                      </div>
+                        </label>
                         </td>
-                        <td>
-                            <?= $item["food_name"] ?>
-                        </td>
-                        <td>
-                            <?= $item["price"] ?>$
-                        </td>
+                        <td> <?= $item["food_name"] ?> </td>
+                        <td> <?= $item["price"] ?>$ </td>
                         <td>
                             <?= $item["quantity"] ?>
                         </td>
@@ -152,9 +154,7 @@
                                             </div>
                                             <div class="form-group col-6">
                                                 <label for="price">Price:</label>
-                                                <input type="number" name="price" id="price" min="0"
-                                                    class="form-control border border-info" value="<?= $item["price"] ?>"
-                                                    required>
+                                                <input type="number" name="price" id="price" min="0" class="form-control border border-info" value="<?= $item["price"] ?>" required>
                                             </div>
                                         </div>
                                         <div class="row d-flex gap-3">
@@ -166,7 +166,7 @@
                                             </div>
                                             <div class="form-group col-6">
                                                 <label for="rating">Rating:</label>
-                                                <input type="number" name="rating" id="food_rate" min="0" max="5"
+                                                <input type="number" name="rating" id="rating" min="0" max="5"
                                                     class="form-control border border-info" value="<?= $item["food_rate"] ?>"
                                                     required>
                                             </div>
@@ -189,24 +189,24 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <!-- delete category -->
-                    <div class="modal fade" id="deletefood<?= $item["food_id"] ?>" tabindex="-1"
-                        aria-labelledby="modal-title" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header row p-2 m-0">
-                                    <div class="alert alert-danger col-12 rounded-0" role="alert">
-                                        Are you sure you want to delete this food?
+                        <!-- delete category -->
+                        <div class="modal fade" id="deletefood<?= $item["food_id"] ?>" tabindex="-1"
+                            aria-labelledby="modal-title" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header row p-2 m-0">
+                                        <div class="alert alert-danger col-12 rounded-0" role="alert">
+                                            Are you sure you want to delete this food?
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="modal-body p-2 m-2 d-flex justify-content-end">
-                                    <button type="button" class="btn btn-secondary mx-2"
-                                        data-bs-dismiss="modal">Close</button>
-                                    <form action="/restaurant/delete_food" method="post">
-                                        <button type="submit" name="food_id" value="<?= $item["food_id"] ?>"
-                                            class="btn btn-primary">Save changes</button>
-                                    </form>
+                                    <div class="modal-body p-2 m-2 d-flex justify-content-end">
+                                        <button type="button" class="btn btn-secondary mx-2"
+                                            data-bs-dismiss="modal">Close</button>
+                                        <form action="/restaurant/delete_food" method="post">
+                                            <button type="submit" name="food_id" value="<?= $item["food_id"] ?>"
+                                                class="btn btn-primary">Save changes</button>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
