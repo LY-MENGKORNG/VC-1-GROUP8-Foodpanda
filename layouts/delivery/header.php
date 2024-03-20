1670aba418329f6f9557a969ca280bcc15c48ea8
MENGKORNG-LY <mengkorng.ly@student.passerellesnumeriques.org>
2024-03-20 17:26:21 +0700

    [feature] in progress on customer order product PD8-15

--- layouts/delivery/header.php
+++ layouts/delivery/header.php
@@ -6,27 +6,28 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <meta name="description" content="Gurdeep Osahan" />
   <meta name="author" content="Gurdeep Osahan" />
-  <title>Delivery | <?= $delivery["first_name"] ?></title>
+  <title>Delivery |
+    <?= $delivery["first_name"] ?>
+  </title>
   <link rel="icon" type="image/png" href="../../assets/images/logo/panda-logo.png" />
 
   <!-- Bootstrap -->
   <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
   <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <!-- Favicon -->
   <link href="../../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-  
-  
+
+
   <link rel="stylesheet" href="../../vendor/css/style.min.css">
   <link href="../../vendor/css/style2.css" rel="stylesheet" />
   <link href="../../vendor/css/style.css" rel="stylesheet" />
   <link href="../../vendor/icons/feather.css" rel="stylesheet" type="text/css" />
   <link href="../../vendor/icons/icons.css" rel="stylesheet" type="text/css" />
 
-</head> 
+</head>
 
 <body>
-    <div class="wrapper position-relative bg-white">
-  
+  <div class="wrapper position-relative bg-white">
