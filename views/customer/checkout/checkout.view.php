69b8a1fefc06000e023476aafc58e96c930ced54
MENGKORNG-LY <mengkorng.ly@student.passerellesnumeriques.org>
2024-03-22 18:37:37 +0700

    [feature] continue on checkout process PD8-150

--- views/customer/checkout/checkout.view.php
+++ views/customer/checkout/checkout.view.php
@@ -159,10 +159,10 @@
                             </div>
                         </div>
                         <!-- add delivery address -->
-                        <div class="modal fade show" id="addAddress" tabindex="-1" role="dialog"
+                        <div class="modal fade" id="addAddress" tabindex="-1" role="dialog"
                             aria-labelledby="addAddressLabel" aria-hidden="true">
                             <div class="modal-dialog d-flex flex-column bg-white">
-                                <form action="/customer/address" id="www" method="post">
+                                <form action="/customer/address" method="post">
                                     <div  class="modal-content">
                                         <div class="modal-header">
                                             <h5 class="modal-title">Add Delivery Address</h5>
