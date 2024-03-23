25e219bb8a0639896cebea9c92435ddcc3949de2
MENGKORNG-LY <mengkorng.ly@student.passerellesnumeriques.org>
2024-03-24 03:50:36 +0700

    [feature] finished on customer reset password PD8-143

--- controllers/authentication/forgot_password/send_password_reset.controller.php
+++ controllers/authentication/forgot_password/send_password_reset.controller.php
@@ -1,19 +1,48 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require './vendor/autoload.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (!empty($_POST["email"])) {
-        $customer_verify = accountExist($_POST["email"], 4);
-        if (count($customer_verify) == 0) {
-            $_SESSION["is_customer_email"] = "Email not found";
-            header("Location: /customer/forgot_password");
-            die();
+
+    $email = $_POST['email'];
+    if (!empty ($email)) {
+        $customer_verify = accountExist($email, 4);
+        $user_name = $customer_verify["first_name"];
+
+        $verify_codes = random_verify_codes();
+        update_reset_token($email, $verify_codes);
+        $currentURL = "http://$_SERVER[HTTP_HOST]";
+        $reset_link = $currentURL . "/customer/change_password";
+
+        $mail = new PHPMailer(true);
+        try {
+            // Server settings
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'lymengkorng54@gmail.com'; // server main
+            $mail->Password = 'loikfrjgqxobxyhm'; // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable implicit TLS encryption
+            $mail->Port = 465; // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            $mail->setFrom('lymengkorng54@gmail.com', 'Food Panda');
+            $mail->addAddress($email);
+
+            // Content
+            $mail->isHTML(true);
+            $mail->Subject = 'Reset Password';
+
+            require "./views/authentication/forgot_password/send_email_form.view.php";
+            $body_content = ob_get_clean();
+            $mail->Body = $body_content;
+            $mail->send();
+            header("Location: /customer/signin");
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
-
-        // send confirmation to customer email
-        $_SESSION["email_sent"] = true;
-        
-
-
-        unset($_SESSION["is_customer_email"]);
-        header("Location: /customer/signin");
     }
 }
